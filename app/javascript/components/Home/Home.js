f174f3ea9337342381cfaf06c1ddb74ba1041ab2
Tanner Noble <tannernoble@gmail.com>
2019-11-30 01:21:42 -0600

    Finished App

---
+++ app/javascript/components/Home/Home.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import Jumbotron from './Jumbotron'
+import Table from './Table/Table'
+import axios from 'axios'
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      course_modules: []
+    }
+  }
+
+  componentDidMount(){
+    axios.get('/episodes.json')
+    .then(data => {
+      debugger
+      let res = []
+
+      data.data.data.map( (data) => {
+        res.push({id: data.id, title: data.title, description: data.description, active: false})
+
+        this.setState({course_modules: res})
+
+      })
+    })
+    .catch(data => {
+      debugger
+    })
+  }
+
+  handleVideoChange(item, event){
+    event.preventDefault()
+
+    let course_modules = [...this.state.course_modules]
+
+    course_modules.map( data => {
+      data.active = false
+    })
+
+    item.active = !item.active
+
+    course_modules[item.id - 1] = item
+
+    this.setState({course_modules})
+    
+  }
+  
+  render() {
+    return (
+      <div>
+        <Jumbotron />
+        <Table handleVideoChange={this.handleVideoChange.bind(this)} course_modules={this.state.course_modules}/>
+      </div>
+    )
+  }
+}
